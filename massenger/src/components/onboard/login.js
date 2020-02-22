57692d277fc27eea855ccb0aa5be245044e218a7
reza <rezamahbobi25@gmail.com>
2020-02-22 23:16:22 +0330

    login page

--- massenger/src/components/onboard/login.js
+++ massenger/src/components/onboard/login.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import Logo from '../../logo.svg';
 class Login extends React.Component {
   render(){
     return(
-      <p>hello </p>
+      <div className='container'>
+        <div className='wraper'>
+          <img className='logo' alt="" src={Logo} />
+          <input type="password"
+          placeholder="Email"
+           />
+          <input 
+          placeholder="Password"/>
+          <button>login</button>
+        </div>
+      </div>
     )
   }
 }
